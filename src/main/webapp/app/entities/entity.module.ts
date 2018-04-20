676ca8cc7cb464b4064cc10ac2beeb0641834521
Monjur Morshed <monjurmorshed793@gmail.com>
2018-04-20 20:40:20 +0600

    Mongodb docker-compose configured. Subject is moved to main navbar.

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,9 +1,11 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { PersonalNotebookSubjectModule } from './subject/subject.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
+        PersonalNotebookSubjectModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
